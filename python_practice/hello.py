23f1b3756fd1d513e2f98400b54b94bc1451586c
naveen yadav <naveeny052@gmail.com>
2026-08-20 12:13:01 +0530

    added few more logic

--- python_practice/hello.py
+++ python_practice/hello.py
@@ -1,1 +1,24 @@
 print("Hello World")
+
+a= 10
+b= 20 
+print(a*b)
+
+
+a =[1,23,45,6,7,89,2,6]
+print(max(a))
+b =sorted(a)
+print(b)
+
+
+dist =[]
+for i in a:
+    if i in dist:
+        pass
+    else:
+       dist.append(i)
+
+print (sorted(dist))
+
+
+a = int(input(f"please eneter number: " ))
